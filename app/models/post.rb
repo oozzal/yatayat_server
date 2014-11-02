90663cc3d3750a920bdf6a22ed9c948bf3967a82
oozzal <theoozzal@gmail.com>
2014-11-02 11:35:40 +0545

    Accessible attributes to post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  attr_accessible :title, :body, :user_id, :sub_category_id
+
   belongs_to :user
   belongs_to :sub_category
   has_one :category, through: :sub_category

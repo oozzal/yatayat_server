18b13006d4f2515320e14a97bdaeccd95affb9e7
oozzal <theoozzal@gmail.com>
2014-11-02 11:09:31 +0545

    Reputation for posts and users.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.1.4'
 gem 'rails-api'
+gem 'activerecord-reputation-system'
+gem 'protected_attributes'
 
 ### OpenShift Online changes:
 
@@ -19,8 +21,8 @@ gem 'rake', '~> 0.9.6'
 #
 group :development, :test do
   gem 'sqlite3'
-  gem 'minitest'
   gem 'thor'
+  gem 'pry'
 end
 
 # Add support for the MySQL
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ActiveRecord::Base
+  attr_accessible :username, :phone_number, :sim_serial_number, :email, :first_name, :last_name, :address
+
+  has_many :posts
+
+  validates :sim_serial_number, presence: true, uniqueness: true
+
+  has_reputation :karma, source: :user
+
+  def like(post)
+    post.add_evaluation(:likes, 1, self)
+    post.author.add_evaluation(:karma, 1, self)
+  rescue
+    false
+  end
+
+  def dislike(post)
+    post.add_evaluation(:dislikes, 1, self)
+    post.author.add_evaluation(:karma, -1, self)
+  rescue
+    false
+  end
+
 end
+

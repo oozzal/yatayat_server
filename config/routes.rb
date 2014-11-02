359c047460bda5d3e90f2002160dbdfd83fc81a4
oozzal <theoozzal@gmail.com>
2014-11-02 11:09:19 +0545

    Retrieve posts from server.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users, except: [:new, :edit]
+  resources :posts, except: [:new, :edit]
+  resources :sub_categories, except: [:new, :edit]
+  resources :categories, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
